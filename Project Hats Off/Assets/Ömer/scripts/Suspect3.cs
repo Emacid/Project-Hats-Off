e2492569f652e2f21ba60ae9a953f42cb54b5f1f
Ömer Erden <omererden08@outlook.com>
2024-10-21 19:05:03 +0300

    SuspectTransition

---
+++ Project Hats Off/Assets/Ömer/scripts/Suspect3.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suspect3 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    private float speed;
+    [SerializeField]
+    private bool isMiddle;
+    [SerializeField]
+    private bool isLeft;
+    [SerializeField]
+    private bool isRight;
+    public Transform middle;
+    public Transform right;
+    public Transform left;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        CheckPosition();
+        CheckMiddle();
+    }
+
+    void CheckPosition()
+    {
+        if (transform.position.x == middle.position.x)
+        {
+            transform.position = middle.position;
+            SuspectManager.instance.isMoving3 = false;
+            isMiddle = true;
+            isLeft = false;
+            isRight = false;
+        }
+        else if (transform.position.x == left.position.x)
+        {
+            transform.position = left.position;
+            isLeft = true;
+            isRight = false;
+            isMiddle = false;
+        }
+        else if (transform.position.x == right.position.x)
+        {
+            transform.position = right.position;
+            isRight = true;
+            isMiddle = false;
+            isLeft = false;
+        }
+    }
+
+    void CheckMiddle()
+    {
+        if (isMiddle)
+        {
+            anim.SetBool("inLeft", false);
+            anim.SetBool("inRight", false);
+
+            if (SuspectManager.instance.isMoving1 || SuspectManager.instance.isMoving2)
+            {
+
+                anim.SetBool("outRight", true);
+            }
+            /* else if ()
+             {
+                 anim.SetBool("outLeft", true);
+             }
+            */
+            isLeft = false;
+            isRight = false;
+        }
+    }
+
+
+    public void MovementSuspect()
+    {
+        anim.SetBool("inLeft", false);
+        anim.SetBool("inRight", false);
+        anim.SetBool("outRight", false);
+        anim.SetBool("outLeft", false);
+
+        if (isLeft)
+        {
+            anim.SetBool("inLeft", true);
+            SuspectManager.instance.isMoving3 = true;
+        }
+        else if (isRight)
+        {
+            anim.SetBool("inRight", true);
+            SuspectManager.instance.isMoving3 = true;
+
+        }
+    }
+
+
+}
+
+
+
---
+++ Project Hats Off/Assets/Ömer/scripts/SuspectManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+public class SuspectManager : MonoBehaviour
+{
+    public GameObject[] suspects;
+    public Suspect suspect;
+    public Suspect2 suspect2;
+    public Suspect3 suspect3;
+
+
+
+    public bool isMoving1;
+    public bool isMoving2;
+    public bool isMoving3;
+
+  
+    public static SuspectManager instance;
+
+
+    private void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+
+
+    public void OnButtonPressed(int buttonIndex)
+    {
+        switch (buttonIndex)
+        {
+            case 0:
+                suspect.MovementSuspect();
+                break;
+            case 1:
+                suspect2.MovementSuspect();
+                break;
+            case 2:
+                suspect3.MovementSuspect();
+                break;
+
+        }
+
+    }
+
+    
+
+
+}
