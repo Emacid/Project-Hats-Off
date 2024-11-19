e642dc4fb2abf6d7ae755cf31e1b133dfff0ab20
Onur Varol <emacidd@gmail.com>
2024-11-19 03:57:50 +0300

    Midterm final build

---
+++ Project Hats Off/Assets/Onur/Scripts/ChooseTheGuilty.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseTheGuilty : MonoBehaviour
+{
+    public GameObject fadeOutBlack;
+    public int suspectNumber = 1;
+    public int RightSuspect = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChooseTheKiller()
+    {
+        if (suspectNumber == RightSuspect)
+        {
+            StartCoroutine(RightKiller());
+        }
+        else if (suspectNumber != RightSuspect)
+        {
+            StartCoroutine(WrongKiller());
+        }
+
+    }
+    public IEnumerator WrongKiller()
+    {
+        fadeOutBlack.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(2);
+    }
+
+    public IEnumerator RightKiller()
+    {
+        fadeOutBlack.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(3);
+    }
+}
--- Project Hats Off/Assets/Onur/Scripts/RestartScene.cs
+++ Project Hats Off/Assets/Onur/Scripts/RestartScene.cs
@@ -9,7 +9,14 @@ public class RestartScene : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R))
         {
             // Mevcut sahneyi yeniden yükler
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene(1);
+        }
+
+        // R tuþuna basýlýp basýlmadýðýný kontrol eder
+        if (Input.GetKeyDown(KeyCode.F8))
+        {
+            // Mevcut sahneyi yeniden yükler
+            SceneManager.LoadScene(4);
         }
     }
 }
